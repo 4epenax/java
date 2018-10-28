b5bac62adfa15b31a8cd90a9fdf7dab399cfb65e
waka <x0bo@ya.ru>
2018-10-28 20:35:07 +0300

    Лекция 1.3. Выражения и операции

--- sandbox/src/main/java/jft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/jft/sandbox/MyFirstProgram.java
@@ -4,8 +4,31 @@ package jft.sandbox;
 //декларация класса, всегда с большой буквы! только класс! всё остальное с маленькой (фигурные скобки - это границы класса)
 public class MyFirstProgram {
 
-    //функция и метод - синонимы, после имени метода всегда есть скобки (фигурные скобки - это границы функции)
+    //функция и метод - синонимы, после имени метода всегда есть скобки (после фигурные скобки - это границы функции)
     public static void main(String[] args) {
+        //вывод на экран (консоль), можно писать команду *sout*
         System.out.println("Hello, world!");
+
+        //арифметика
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        //через точку (плавающая точка) пишутся дробные числа
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+        System.out.println(2.0 / 2);
+
+
+        //в кавычках выделяются строки и их можно склеивать (операция конкатенации)
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + "2");
+
+        System.out.println((2 + 2) * 2);
+
+        System.out.println("2 + 2 = " + (2 + 2));
     }
 }
